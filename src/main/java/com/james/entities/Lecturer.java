0ecfab3bce262b56cf7bae868fd4a7ee6ecad829
jamesyburr <vint.james@gmail.com>
2016-07-01 13:01:17 -0400

    initial import: created controllers, entities and services packages

---
+++ src/main/java/com/james/entities/Lecturer.java
@@ -0,0 +1,7 @@
+package com.james.entities;
+
+/**
+ * Created by jamesyburr on 7/1/16.
+ */
+public class Lecturer {
+}
---
+++ src/main/java/com/james/services/LecturerRepository.java
@@ -0,0 +1,7 @@
+package com.james.services;
+
+/**
+ * Created by jamesyburr on 7/1/16.
+ */
+public interface LecturerRepository {
+}
