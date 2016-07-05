e407c249a3adbc418de0b7e13a89ee49f52977f9
jamesyburr <vint.james@gmail.com>
2016-07-05 08:21:53 -0400

    completed post routes

--- src/main/java/com/james/controllers/HLController.java
+++ src/main/java/com/james/controllers/HLController.java
@@ -1,7 +1,37 @@
 package com.james.controllers;
 
+import com.james.entities.Lecturer;
+import com.james.entities.Review;
+import com.james.services.LecturerRepository;
+import com.james.services.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by jamesyburr on 7/1/16.
  */
+@Controller
 public class HLController {
+
+    @Autowired
+    LecturerRepository lecturers;
+
+    @Autowired
+    ReviewRepository reviews;
+
+    @RequestMapping(path = "/lecturers", method = RequestMethod.POST)
+    public String addLctr (String name, String topic, String image) {
+        Lecturer lctr = new Lecturer(name, topic, image);
+        lecturers.save(lctr);
+        return "index";
+    }
+
+    @RequestMapping(path = "/reviews", method = RequestMethod.POST)
+    public String addRvw (String author, String text, boolean isGood, int lecturerId) {
+        Review rvw = new Review(author, text, isGood, lecturers.findOne(lecturerId));
+        reviews.save(rvw);
+        return "index";
+    }
 }
--- src/main/java/com/james/controllers/HLRestController.java
+++ src/main/java/com/james/controllers/HLRestController.java
@@ -1,7 +1,35 @@
 package com.james.controllers;
 
+
+import com.james.entities.Lecturer;
+import com.james.entities.Review;
+import com.james.services.LecturerRepository;
+import com.james.services.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by jamesyburr on 7/1/16.
  */
+@RestController
 public class HLRestController {
+
+    @Autowired
+    LecturerRepository lecturers;
+
+    @Autowired
+    ReviewRepository reviews;
+
+    @RequestMapping(path = "/lecturers", method = RequestMethod.GET)
+    public Iterable<Lecturer> lctrs () {
+        return lecturers.findAll();
+    }
+
+    @RequestMapping(path = "/reviews", method = RequestMethod.GET)
+    public Iterable<Review> rvws () {
+        return reviews.findAll();
+    }
 }
+
